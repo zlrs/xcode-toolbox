9743e0938736e325ae0a3f515a33e8cc20e9f8b4
朱元清 <zhuyuanqing@bytedance.com>
2020-07-20 14:13:48 +0800

    add same file check to installation script

--- install.sh
+++ install.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 install_to_path="/usr/local/bin/xc"
+
 echo "Installing xc command to $install_to_path"
 cp ./xc $install_to_path
-if [ -f "$install_to_path" ]; then
+
+if ([ -f "$install_to_path" ] && [ `md5sum ./xc | cut -c 1-32` == `md5sum $install_to_path | cut -c 1-32` ]) then
     echo "Installation has been completed. "
 else
     echo "Installation failed. "
 fi
-
