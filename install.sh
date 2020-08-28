671714e7670896dc5a3fbab2a3b5e2a5eb542f55
zlrs <zhuys123@gmail.com>
2020-08-28 22:01:46 +0800

    Version update check (#2)
    * update gitignore
    
    * add version check script file
    
    * 将version_check代码合入xc命令
    
    * 升级版本号; 修改安装脚本
    
    * remvoe .vscode from source control

--- install.sh
+++ install.sh
@@ -1,11 +1,66 @@
-#!/bin/bash
-install_to_path="/usr/local/bin/xc"
+#!/bin/zsh
 
-echo "Installing xc command to $install_to_path"
-cp ./xc $install_to_path
+repo_folder="/usr/local/xc"
+command_path="/usr/local/bin/xc"
 
-if ([ -f "$install_to_path" ] && [ `md5sum ./xc | cut -c 1-32` == `md5sum $install_to_path | cut -c 1-32` ]) then
-    echo "Installation has been completed. "
-else
-    echo "Installation failed. "
+# ANSI colors
+RED='\033[0;31m'
+GREEN='\033[0;32m'
+PURPLE='\033[0;35m'
+NC='\033[0m' # No Color
+
+Error="${RED}[Error] "
+Info="${GREEN}[Info] "
+Execute="${PURPLE}[Execute] "
+
+
+if [ ! -d "/usr/local/xc" ]  # 不存在xc文件夹
+then
+    # clone repo
+    echo "${Execute}git clone https://github.com/zlrs/xcode-opener.git $repo_folder ${NC}"
+    git clone https://github.com/zlrs/xcode-opener.git $repo_folder
+    
+    # gen symbol link
+    echo "${Info}Installing xc command to $command_path ${NC}"
+    # 如果路径上已有文件，则删掉
+    if [ -f $command_path ] 
+    then
+        echo "${Execute}rm $command_path ${NC}"
+        rm $command_path
+    fi
+    echo "${Execute}ln -s /usr/local/xc/xc $command_path ${NC}"
+    ln -s /usr/local/xc/xc $command_path
+    
+    echo "${Info}Installation has been completed. ${NC}"
+else  # 存在 xc 文件夹
+    if [ -d "/usr/local/xc/.git" ]  # 且 xc 文件夹是个git repo
+    then
+        echo "${Info}Already installed xc. Updating... ${NC}"
+        
+        echo "${Execute}cd /usr/local/xc${NC}"
+        cd /usr/local/xc
+        
+        echo "${Execute}git pull${NC}"
+        git pull
+        
+        if [ -f $command_path ] 
+        then
+            echo "${Execute}rm $command_path ${NC}"
+            rm $command_path
+        fi
+        
+        echo "${Execute}ln -s /usr/local/xc/xc $command_path ${NC}"
+        ln -s /usr/local/xc/xc $command_path
+    else
+        echo "${Error}/usr/local/xc/ exists and is not a git repository. ${NC}"
+        echo "${Error}No operation is performed. ${NC}"
+        echo "${Error}Maybe you can consider remove the folder and install again?${NC}"
+        exit 1
+    fi
 fi
+
+# if ([ -f "$command_path" ] && [ `md5sum ./xc | cut -c 1-32` == `md5sum $command_path | cut -c 1-32` ]) then
+#     echo "Installation has been completed. "
+# else
+#     echo "Installation failed. "
+# fi
---
+++ version_check.py
@@ -0,0 +1,85 @@
+import requests
+import os
+import json
+
+VERSION = '1.1.0'
+
+
+class Version:
+    def __init__(self, version: str):
+        version.strip()
+        version.strip("vV")
+        self.version = version
+        self.sematic_versions = version.split('.')
+        self.digitVersions = []
+        for sematic_ver in self.sematic_versions:
+            self.digitVersions.append(int(sematic_ver))
+    
+    def __eq__(self, other):
+        return tuple(self.digitVersions) == tuple(other.digitVersions)
+    
+    def __lt__(self, other):
+        return tuple(self.digitVersions) < tuple(other.digitVersions)
+
+
+def get_latest_version():
+    URL = 'https://api.github.com/repos/zlrs/xcode-opener/releases?accept=application/vnd.github.v3+json'
+    res = requests.get(URL)
+    if not res.ok:
+        return '', "error"
+    
+    try:
+        obj = res.json()
+        latest_version = obj[0]["tag_name"]
+        return latest_version, None
+    except:
+        return '', "error"
+
+
+def version_check():
+    latestVersionStr, err = get_latest_version()
+    if not err:
+        current = Version(VERSION)
+        latest = Version(latestVersionStr)
+        if latest > current:
+            print('You are using xc %s. The latest version is %s. Please consider upgrade. ' % (current.version, latest.version))
+            print('https://github.com/zlrs/xcode-opener/releases')
+
+
+def shouldCheckVersion():
+    def initVersionCheck(file_path):
+        data = {
+            "version_check_count": 1
+        }
+        with open(file_path, 'w', encoding='utf-8') as f:
+            json.dump(data, f)
+
+    folder = os.path.join(os.path.expanduser('~'), '.xc')
+    if not os.path.exists(folder):
+        os.mkdir(folder)
+    
+    file_path = os.path.join(folder, 'version_check.json')
+    if not os.path.exists(file_path):
+        initVersionCheck(file_path)
+    else:
+        try:
+            with open(file_path, 'r+', encoding='utf-8') as f:
+                data = json.load(f)
+                data["version_check_count"] += 1
+                f.seek(0)
+                json.dump(data, f)
+        except:
+            initVersionCheck(file_path)
+    
+    if data["version_check_count"] % 10 == 0:
+        return True
+    return False
+
+
+def run():
+    if shouldCheckVersion():
+        version_check()
+
+
+if __name__ == '__main__':
+    run()
