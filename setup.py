0cb066d399d9dc7cbeac1b5c81376d454bfec5e0
zlrs <zhuys123@gmail.com>
2020-09-04 13:56:27 +0800

    1. setup.py: description should be one line 2. setup.py: specify `include_package_data` and add file MANIFEST.in, see  https://stackoverflow.com/questions/23174516/read-readme-in-setup-py 3. bump version to 1.2.3

--- setup.py
+++ setup.py
@@ -5,18 +5,11 @@ from xc import __version__
 
 version = __version__
 
-description = """"A CLI tool which aims to provide a convenient operation toolbox on XCode project.
-    You can use it to:
-    (1) open XCode project or workspace. (2) remove project's derived data.
-    (3) [WIP] force kill XCode process. (4) [WIP] generate Objective-C function signatures.
+description = "A CLI tool which aims to provide a convenient operation toolbox on XCode project. You can use it to:" \
+    "(1) open XCode project or workspace. (2) remove project's derived data." \
+    "(3) [WIP] force kill XCode process. (4) [WIP] generate Objective-C function signatures."
 
-    The argument `path` is a path to the directory containing at least 1 `.xcodeproj` or `.xcworkspace` file, default
-    to the current directory. `.xcworkspace` has higher priority than `.xcodeproj`.
-
-    Contribute: https://github.com/zlrs/xcode-opener
-    """
-
-with open('readme.rst', encoding='utf-8') as fp:
+with open('readme.md', encoding='utf-8') as fp:
     long_description = fp.read()
 
 install_requires = []
@@ -30,11 +23,12 @@ setup(
     version=version,
     description=description,
     long_description=long_description,
-    long_description_content_type='text/markdown',
+    long_description_content_type="text/markdown",
     author="zlrs",
     author_email="zhuys123@gmail.com",
     url="https://github.com/zlrs/xcode-opener",
 
+    include_package_data=True,
     packages=["xc"],
     entry_points={
         "console_scripts": ['xc = xc.xc:xc']  # xc.xc:xc  xc module - xc.py file - xc function
